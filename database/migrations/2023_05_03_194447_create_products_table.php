6071a824693cd818298be10e280da5a50b5708e1
Yohanes Cahyadi <yohanescahyadi59@gmail.com>
2023-05-04 03:41:35 +0700

    feat: create products migration file

---
+++ database/migrations/2023_05_03_194447_create_products_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->uuid('categories_id')->nullable();
+            $table->foreign('categories_id')->references('id')->on('categories')->cascadeOnDelete();
+            $table->string('name');
+            $table->string('image');
+            $table->integer('price')->unsigned()->default(0);
+            $table->text('description');
+            $table->integer('weight')->unsigned();
+            $table->string('slug');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
