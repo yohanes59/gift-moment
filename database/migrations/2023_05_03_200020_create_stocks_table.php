d813fae6500cb878e80bde3f9fba73bb377502a0
Yohanes Cahyadi <yohanescahyadi59@gmail.com>
2023-05-04 03:42:08 +0700

    feat: create stocks migration file

---
+++ database/migrations/2023_05_03_200020_create_stocks_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stocks', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->uuid('products_id');
+            $table->foreign('products_id')->references('id')->on('products')->cascadeOnDelete();
+            $table->integer('stock_in')->unsigned()->nullable();
+            $table->integer('stock_out')->unsigned()->nullable();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('stocks');
+    }
+};
