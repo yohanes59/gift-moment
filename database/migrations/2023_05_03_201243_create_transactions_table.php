8acc23ae8744fc9471ba53c48f7b7a10f451f8ec
Yohanes Cahyadi <yohanescahyadi59@gmail.com>
2023-05-04 03:42:27 +0700

    feat: create transactions migration file

---
+++ database/migrations/2023_05_03_201243_create_transactions_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->uuid('users_id');
+            $table->foreign('users_id')->references('id')->on('users');
+            $table->integer('total')->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
