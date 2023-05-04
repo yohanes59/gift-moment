5000bea3c80b2007b37990f528d453030b42471f
Yohanes Cahyadi <yohanescahyadi59@gmail.com>
2023-05-05 04:34:11 +0700

    feat: create model file for user data

---
+++ app/Models/UserData.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class UserData extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $guarded = [];
+    
+    public function getIncrementing()
+    {
+        return false;
+    }
+
+    public function getKeyType()
+    {
+        return 'string';
+    }
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        self::creating(function ($model) {
+            $model->{$model->getKeyName()} = Str::uuid()->toString();
+        });
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'users_id', 'id');
+    }
+}
