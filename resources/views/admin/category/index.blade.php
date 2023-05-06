31b33c10a11fdb31be3f3b6b230c243189b83b26
Yohanes Cahyadi <yohanescahyadi59@gmail.com>
2023-05-07 02:29:47 +0700

    feat: create confirm on delete category item

--- resources/views/admin/category/index.blade.php
+++ resources/views/admin/category/index.blade.php
@@ -4,47 +4,6 @@
 
 @section('admin')
     <div>
-        {{-- MODAL DELETE ITEM --}}
-        <div id="deleteModal" tabindex="-1"
-            class="fixed top-0 left-0 right-0 z-50 hidden p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-modal md:h-full">
-            <div class="relative w-full h-full max-w-md md:h-auto">
-                <div class="relative bg-white rounded-lg shadow">
-                    <button type="button"
-                        class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center"
-                        data-modal-toggle="deleteModal">
-                        <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20"
-                            xmlns="http://www.w3.org/2000/svg">
-                            <path fill-rule="evenodd"
-                                d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                                clip-rule="evenodd"></path>
-                        </svg>
-                        <span class="sr-only">Close modal</span>
-                    </button>
-                    <div class="p-6 text-center">
-                        <svg aria-hidden="true" class="mx-auto mb-4 text-gray-400 w-14 h-14" fill="none"
-                            stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
-                        </svg>
-
-                        {{-- FORM FOR DELETE ITEM --}}
-                        <form action="" method="POST">
-                            @csrf
-                            <input type="text" class="w-full" name="deleted_id" id="delete_id">
-                            <h3 class="mb-5 text-lg font-normal text-gray-500">Apakah Anda yakin ingin menghapus kategori
-                                ini?</h3>
-                            <button data-modal-toggle="deleteModal" type="button"
-                                class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10">Batal</button>
-                            <button id="hapus-berita" type="submit" data-modal-toggle="deleteModal"
-                                class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center ml-2">
-                                Yakin
-                            </button>
-                        </form>
-
-                    </div>
-                </div>
-            </div>
-        </div>
 
         <div class="text-xl font-medium mb-8">List Kategori</div>
 
@@ -85,7 +44,7 @@
 
         {{-- Table --}}
         <div class="overflow-x-auto relative shadow-md sm:rounded-md">
-            <table class="w-full text-sm text-left text-gray-500">
+            <table class="w-full text-sm text-left text-gray-500" id="crudTable">
                 <thead class="text-xs text-white uppercase bg-indigo-500">
                     <tr class="divide-x divide-y">
                         <th scope="col" class="w-16 py-3 px-6">
@@ -103,41 +62,83 @@
                     </tr>
                 </thead>
                 <tbody class="bg-white divide-y">
-                    @foreach ($category as $item)
-                        <tr class="divide-x">
-                            <td class="w-16 py-4 px-6 text-center font-normal text-gray-900 whitespace-nowrap">
-                                {{ $item->id }}
-                            </td>
-                            <td class="py-4 px-6">
-                                <img src="{{ Storage::url($item->image) }}" alt="image {{ $item->name }}">
-                            </td>
-                            <td class="py-4 px-6">
-                                {{ $item->name }}
-                            </td>
-                            <td class="w-32 py-4 px-6">
-                                <div class="flex items-center space-x-2">
-                                    <a href="#" class="py-2 px-3 rounded-md text-white bg-blue-500 hover:bg-blue-600">
-                                        <i class="fa-solid fa-arrow-up-right-from-square"></i>
-                                    </a>
-                                    <a href="/admin/category/{{ $item->id }}/edit"
-                                        class="py-2 px-3 rounded-md text-white bg-yellow-500 hover:bg-yellow-600">
-                                        <i class="fa-solid fa-edit"></i>
-                                    </a>
-                                    <form action="{{ url('admin/category/' . $item->id) }}" method="POST">
-                                        @method('DELETE')
-                                        @csrf
-                                        <button type="submit"
-                                            class="py-2 px-3 rounded-md text-white bg-red-500 hover:bg-red-600"
-                                            onclick="return('Ingin Menghapus data ini?')">
-                                            <i class="fa-solid fa-trash"></i>
-                                        </button>
-                                    </form>
-                                </div>
-                            </td>
-                        </tr>
-                    @endforeach
+
                 </tbody>
             </table>
         </div>
     </div>
 @endsection
+
+@push('addon-script')
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <script>
+        var datatable = $('#crudTable').DataTable({
+            processing: true,
+            serverSide: true,
+            ordering: true,
+            ajax: {
+                url: '{!! url()->current() !!}',
+            },
+            columns: [{
+                    data: 'id',
+                    name: 'id',
+                    className: 'w-16 py-4 px-6 text-center font-normal text-gray-900 whitespace-nowrap',
+                    width: '20%'
+                },
+                {
+                    data: 'image',
+                    name: 'image',
+                    className: 'py-4 px-6',
+                    width: '20%'
+                },
+                {
+                    data: 'name',
+                    name: 'name',
+                    className: 'py-4 px-6',
+                    width: '20%'
+                },
+                {
+                    data: 'action',
+                    name: 'action',
+                    orderable: false,
+                    searchable: false,
+                    width: '15%'
+                },
+            ]
+        })
+
+        function deleteData(itemId) {
+            let table = $('#crudTable');
+            Swal.fire({
+                title: 'Are you sure?',
+                text: "You won't be able to revert this!",
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Yes, delete it!'
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    let url = '{{ url('admin/category') }}/' + itemId;
+
+                    $.ajax({
+                        type: 'POST',
+                        url: url,
+                        data: {
+                            '_method': 'DELETE',
+                            '_token': '{{ csrf_token() }}'
+                        },
+                        success: function() {
+                            Swal.fire(
+                                'Deleted!',
+                                'Your file has been deleted.',
+                                'success'
+                            )
+                            table.DataTable().ajax.reload()
+                        }
+                    })
+                }
+            })
+        }
+    </script>
+@endpush
--- resources/views/layouts/app-admin.blade.php
+++ resources/views/layouts/app-admin.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,14 +9,17 @@
 
     @include('include.style')
 </head>
+
 <body>
     @include('include.admin.sidebar')
 
     <div class="p-6 sm:ml-64 mt-4">
         @yield('admin')
     </div>
-    
+
     @include('include.script')
+    @stack('addon-script')
     @include('sweetalert::alert')
 </body>
+
 </html>
