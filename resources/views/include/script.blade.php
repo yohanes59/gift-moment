326090f03ef83386eed2572dc5dce3102456ca6c
Yohanes Cahyadi <yohanescahyadi59@gmail.com>
2023-05-07 01:31:05 +0700

    feat: install styling and script for yajra

--- resources/views/include/script.blade.php
+++ resources/views/include/script.blade.php
@@ -1,5 +1,6 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.4/flowbite.min.js"></script>
 <script type="text/javascript" src="//code.jquery.com/jquery-1.11.0.min.js"></script>
 <script type="text/javascript" src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+<script src="https://cdn.datatables.net/v/bs5/dt-1.13.4/datatables.min.js"></script>
 
 <script src="{{ asset('assets/js/imgPreview.js') }}"></script>
