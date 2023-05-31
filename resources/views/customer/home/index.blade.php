2384fc38ec9f3bff00d1f78a69be3245da041247
awaqo <aqiljawadalfurqon@gmail.com>
2023-05-31 12:49:05 +0700

    feat: add search field at home user

--- resources/views/customer/home/index.blade.php
+++ resources/views/customer/home/index.blade.php
@@ -4,7 +4,7 @@
 
 @section('navbar')
     @include('include.customer.navbar')
-    <div class="mt-[73px] md:mt-[66px] lg:mt-16"></div>
+    <div class="mt-8 md:mt-[66px] lg:mt-16"></div>
 @endsection
 
 @section('content')
@@ -45,11 +45,30 @@
 
         {{-- Produk --}}
         <section class="py-4 px-4">
-            <div class="text-xl md:text-2xl font-bold text-slate-900">Produk</div>
+            <div class="flex flex-col gap-3 sm:gap-0 sm:flex-row sm:justify-between sm:items-center">
+                <div class="text-xl md:text-2xl font-bold text-slate-900">Produk</div>
+                <div class="w-full max-w-sm lg:max-w-lg">
+                    {{-- search --}}
+                    <form>   
+                        <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only">Search</label>
+                        <div class="relative">
+                            <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                                <svg aria-hidden="true" class="w-5 h-5 text-gray-500" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
+                            </div>
+                            <input type="search" id="default-search" class="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-indigo-500 focus:border-indigo-500" placeholder="Cari produk impianmu..." required>
+                            <button type="submit" class="text-white absolute right-2.5 bottom-2.5 bg-indigo-700 hover:bg-indigo-800 focus:ring-4 focus:outline-none focus:ring-indigo-300 font-medium rounded-lg text-sm px-4 py-2">
+                                <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
+                                <span class="sr-only">Search</span>
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+
             <div class="flex flex-wrap gap-5 mt-3 mb-8">
                 @foreach ($products as $item)
                     <a href="{{ url('/product/' . $item->slug) }}"
-                        class="w-full max-w-[150px] lg:max-w-[270px] md:max-w-[220px] flex flex-col gap-2 pb-2 shadow-lg overflow-hidden rounded-md">
+                        class="w-full max-w-[150px] md:max-w-[220px] lg:max-w-[290px] xl:max-w-[276px] flex flex-col gap-2 pb-2 border border-indigo-50 shadow-lg shadow-indigo-100 overflow-hidden rounded-md">
                         <div class="w-full h-48 md:h-72 lg:h-80">
                             @if ($item->image != '')
                                 <img src="{{ asset(Storage::url($item->image)) }}"
@@ -87,7 +106,6 @@
                 <div class="space-y-3">
                     @foreach ($faq as $item)
                         <div>
-
                             <h2 id="faq-heading-{{ $item->id }}">
                                 <button type="button"
                                     class="bg-white flex items-center justify-between w-full p-5 font-medium text-left text-slate-900"
